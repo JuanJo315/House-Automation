7def0a9539b0e07fa5c14bf9771e8b478f8c1fce
JuanJo315 <133546097+JuanJo315@users.noreply.github.com>
2023-05-20 23:59:08 +0200

    Delete src/main directory

--- src/main/java/com/example/smarthomeapp/ui/data/HomeUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.smarthomeapp.ui.data
-
-data class HomeUiState (
-    val currentLightState: Boolean = false,
-    val currentTemperature: Int = 0,
-    val currentHumidity: Int = 0,
-    val chatBotUsed: Boolean = false,
-    val lastChatText: String = ""
-)
